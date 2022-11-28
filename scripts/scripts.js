24255ab0af31b83363b664ae086250fe17ac75d8
Louie <causingljbcit@gmail.com>
2022-11-27 20:19:54 -0800

    removed id and switched to class to not have cascading issues when query selecting

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,11 @@
-const darkTheme = document.querySelector(.dark_theme)
+const darkTheme = document.querySelector(".dark_theme");
+const universal = document.
 
+
+darkTheme.addEventListener('click',dark_mode);
+
+function dark_mode(){
+    darkTheme.classList.toggle("gray_color")
+    document.classList.toggle("white_font")
+
+}
