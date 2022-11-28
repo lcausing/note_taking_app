29dd39b8bead1f6c839b0f0332eadee11126e681
Louie <causingljbcit@gmail.com>
2022-11-28 11:29:30 -0800

    moved the font to the body so that the adding the class works

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,7 @@ const darkThemeButton = document.querySelector(".dark_theme_button");
 const text_area = document.querySelector(".text_area")
 const aside_area = document.querySelector(".aside_area")
 const body_area = document.querySelector(".body_area")
-const buttons = document.querySelector()
+const buttons = document.querySelector(".button")
 
 darkThemeButton.addEventListener('click',dark_mode);
 
@@ -10,4 +10,6 @@ function dark_mode(){
     text_area.classList.toggle("gray_bg_color")
     aside_area.classList.toggle("gray_bg_color")
     body_area.classList.toggle("black_bg_color")
+    body_area.classList.toggle("white_font_color")
+    buttons.classList.toggle("white_font_color")
 }
