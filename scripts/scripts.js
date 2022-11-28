4bd9f8110f1de57ec41fd5dd992b2357f4d2e457
Louie <causingljbcit@gmail.com>
2022-11-28 11:21:37 -0800

    made background for aside and body dark when dark theme button is clicked

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,13 @@
 const darkThemeButton = document.querySelector(".dark_theme_button");
 const text_area = document.querySelector(".text_area")
+const aside_area = document.querySelector(".aside_area")
+const body_area = document.querySelector(".body_area")
+const buttons = document.querySelector()
 
 darkThemeButton.addEventListener('click',dark_mode);
 
 function dark_mode(){
     text_area.classList.toggle("gray_bg_color")
-    
+    aside_area.classList.toggle("gray_bg_color")
+    body_area.classList.toggle("black_bg_color")
 }
