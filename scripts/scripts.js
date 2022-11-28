0caef201be8d9b63d27f928050dfc75cff9514a2
Louie <causingljbcit@gmail.com>
2022-11-28 12:40:06 -0800

    modified dark theme colors for text area, and font; changed grouping for css dark theme

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,14 +2,11 @@ const darkThemeButton = document.querySelector(".dark_theme_button");
 const text_area = document.querySelector(".text_area")
 const aside_area = document.querySelector(".aside_area")
 const body_area = document.querySelector(".body_area")
-const buttons = document.querySelector(".button")
 
 darkThemeButton.addEventListener('click',dark_mode);
 
 function dark_mode(){
-    text_area.classList.toggle("gray_bg_color")
-    aside_area.classList.toggle("gray_bg_color")
-    body_area.classList.toggle("black_bg_color")
-    body_area.classList.toggle("white_font_color")
-    buttons.classList.toggle("white_font_color")
+    text_area.classList.toggle("dark_mode_text_area")
+    aside_area.classList.toggle("dark_mode_aside")
+    body_area.classList.toggle("dark_mode_body")
 }
